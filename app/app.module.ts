f4da1ec749a1081343a20c20c57600233a45a532
Sheena Nickerson and Timothy Herold <sheena+timothy@>
2016-10-18 15:26:59 -0700

    Add project

---
+++ app/app.module.ts
@@ -0,0 +1,39 @@
+import './rxjs-extensions';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }     from './dashboard.component';
+import { HeroService }         from './hero.service';
+import { HeroSearchComponent} from './hero-search.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
